73b65695c9efc0c859736f90af4491894a97075c
Mikhail Novik <novik.misha.1@gmail.com>
2023-08-11 00:36:49 +0300

    fix context circular

--- src/main/kotlin/com/noverin/drinker/service/usecase/StartDrinkUseCase.kt
+++ src/main/kotlin/com/noverin/drinker/service/usecase/StartDrinkUseCase.kt
@@ -4,10 +4,12 @@ import com.noverin.drinker.domain.DrinkerService
 import com.noverin.drinker.domain.drink
 import com.noverin.drinker.infrastructure.util.withMachine
 import com.noverin.drinker.service.repository.TwitchUserRepository
+import org.springframework.context.annotation.Lazy
 import org.springframework.stereotype.Component
 
 @Component
 class StartDrinkUseCase(
+    @Lazy
     val drinkerService: DrinkerService,
     val twitchUserRepository: TwitchUserRepository
 ) {
