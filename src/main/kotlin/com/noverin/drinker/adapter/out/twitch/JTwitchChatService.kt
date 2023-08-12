491429d4aefd9f353d293e3bcad7c3b8d034355b
Mikhail Novik <novik.misha.1@gmail.com>
2023-08-12 21:40:22 +0300

    add dirty code for assume that message was send in chat

--- src/main/kotlin/com/noverin/drinker/adapter/out/twitch/JTwitchChatService.kt
+++ src/main/kotlin/com/noverin/drinker/adapter/out/twitch/JTwitchChatService.kt
@@ -17,7 +17,11 @@ class JTwitchChatService : TwitchChatService {
             .build()
             .use { chat ->
                 chat.sendMessage(streamerUsername, message)
+                // todo do something with this
+                // i have to add some await till send message task will taken by taskExecutor
+                // and with that twitchChat.close() will wait till all tasks will be done
+                // and close only after that
+                Thread.sleep(700L)
             }
-
     }
 }
